84c246857183a832564414535ae2013ad2705f19
jgchenu <jgchenu@foxmail.com>
2018-09-02 00:51:39 +0800

    增加了底部tab

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -24,15 +24,7 @@
         </swiper-item>
       </swiper>     
 
-      <div class="rel">
-           <div class="relButton" :style="{transform:'rotate('+(openRel?45:0)+'deg) scale('+(openRel?1:.6)+')',opacity:(openRel?1:.6)}" @click="()=>{this.openRel=!this.openRel}">
-             <img src="/static/images/bottom/release.png" alt="">
-           </div>
-           <div class="buttons" :style="{transform:'translate(0,'+(openRel?0:100)+'px) scale('+(openRel?1:0)+')',opacity:(openRel?1:0)}">
-              <div class="relTalk" @click="relTalk"><img src="/static/images/bottom/talk.png" alt=""><div class="talkText">说说</div></div>
-              <div class="relTask" @click="relTask"><img src="/static/images/bottom/task.png" alt=""><div class="taskText">任务</div></div>
-           </div>
-       </div>  
+
 
         <!-- <tabMenu></tabMenu> -->
   </div>
@@ -46,7 +38,6 @@ import talkList from "../../components/talkList";
 import taskList from "../../components/taskList";
 import Tabs from "../../components/tabs";
 // import tabMenu from "../../components/tabMenu";
-
 export default {
   onReachBottom() {
     console.log("到底了");
@@ -142,59 +133,7 @@ export default {
       height: 100%;
     }
   }
-  .rel {
-    width: 750rpx;
-    position: fixed;
-    bottom: 0;
-    .relButton {
-      background-color: #ffffff;
-      position: absolute;
-      bottom: 0;
-      z-index: 3;
-      width: 100rpx;
-      height: 100rpx;
-      left: 325rpx;
-      border-radius: 50%;
-      border: 4rpx dotted $identityBg;
-      transition: transform 0.8s, opacity 0.8s;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      img {
-        width: 90%;
-        height: 90%;
-      }
-    }
-    .buttons {
-      z-index: 2;
-      position: absolute;
-      bottom: 50rpx;
-      width: 340rpx;
-      height: 200rpx;
-      left: 205rpx;
-      border: 4rpx dotted $identityBg;
-      border-radius: 80rpx;
-      transition: transform 0.8s, opacity 0.8s;
-      background-color: #ffffff;
-      display: flex;
-      justify-content: space-around;
-      align-items: flex-start;
-      .relTalk {
-        margin-top: 10rpx;
-        flex: 1;
-        text-align: center;
-        border-right: 4rpx dotted $identityBg;
-        img {
-          width: 100rpx;
-          height: 100rpx;
-        }
-      }
-      .relTask {
-        @extend .relTalk;
-        border-right: none;
-      }
-    }
-  }
+
 }
 </style>
 
---
+++ src/pages/rel/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+      <div class="buttons">
+              <div class="relTalk" @click="relTalk"><img src="/static/images/bottom/talk.png" alt=""><div class="talkText">说说</div></div>
+              <div class="relTask" @click="relTask"><img src="/static/images/bottom/task.png" alt=""><div class="taskText">任务</div></div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+
+  data() {
+    return {};
+  },
+
+  created() {}
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../style/vars.scss";
+.container {
+  width: 100%;
+  .buttons {
+    width: 340rpx;
+    height: 200rpx;
+    border: 4rpx dotted $identityBg;
+    border-radius: 80rpx;
+    transition: transform 0.8s, opacity 0.8s;
+    background-color: #ffffff;
+    display: flex;
+    justify-content: space-around;
+    align-items: flex-start;
+    .relTalk {
+      margin-top: 10rpx;
+      flex: 1;
+      text-align: center;
+      border-right: 4rpx dotted $identityBg;
+      img {
+        width: 100rpx;
+        height: 100rpx;
+      }
+    }
+    .relTask {
+      @extend .relTalk;
+      border-right: none;
+    }
+  }
+}
+</style>
+
